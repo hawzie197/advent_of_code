d7bc353c7f25581cd3c38bbc154ae26128d03f57
Michael Hawes <mhawes24@gmail.com>
2016-12-03 01:23:56 -0500

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,41 @@
+# Author: Michael Hawes
+# Date: 3 December 2016
+# day3.py
+
+class Triangles:
+
+    def check_if_valid(self, data):
+        """Checks to see if the sum of any two sides is larger than the remaining side"""
+        count = 0
+        for numbers in data:
+            sum_one = int(numbers[0]) + int(numbers[1])  # remaining side = 3
+            sum_two = int(numbers[0]) + int(numbers[2])  # remaining side = 2
+            sum_three = int(numbers[1]) + int(numbers[2])  # remaining side = 1
+            if sum_one > int(numbers[2]) and sum_two > int(numbers[1]) and sum_three > int(numbers[0]):
+                count += 1
+            else:
+                pass
+        return count
+
+
+    def read_file(self):
+        """Reads in the file and formats to appropriate form"""
+        num_list = []
+        infile = open('input.txt', 'r')
+        infile = infile.readlines()
+        for line in infile:
+            line = line.rstrip('\n')
+            line = line.split()
+            num_list.append(line)
+        return num_list
+
+
+def main():
+    T = Triangles()
+
+    data = T.read_file()
+    #data = ['5', '10', '25']
+    print(T.check_if_valid(data))
+
+if __name__ == '__main__':
+    main()
